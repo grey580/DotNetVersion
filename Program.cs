a93e899862e5bde8b8de12636cd09bf0c6cdfab3
grey580 <grey580@yahoo.com>
2018-08-22 09:23:08 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetVersion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Version: {Environment.Version}");
+            Console.WriteLine("Press Enter to close.");
+            Console.ReadKey();
+        }
+    }
+}
